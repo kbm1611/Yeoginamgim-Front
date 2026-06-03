97ea47dccfa62135a1e27c81423be1d61f73d1b3
kbm1611 <bm1611@naver.com>
2026-06-03 11:11:27 +0900

    RequireAuth 추가

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,6 @@
-﻿import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { getAuthToken } from './api/client'
 import MainLayout from './layouts/MainLayout'
 import BoardDetail from './pages/BoardDetail'
 import HomePage from './pages/HomePage'
@@ -10,6 +11,16 @@ import PlaceDetail from './pages/PlaceDetail'
 import PostItEditor from './pages/PostItEditor'
 import SignupPage from './pages/SignupPage'
 import SplashPage from './pages/SplashPage'
+
+function RequireAuth({ children }) {
+  const location = useLocation()
+
+  if (!getAuthToken()) {
+    return <Navigate to="/login" replace state={{ from: location }} />
+  }
+
+  return children
+}
 
 function AnimatedRoutes() {
   const location = useLocation()
@@ -23,7 +34,13 @@ function AnimatedRoutes() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        <Route element={<MainLayout />}>
+        <Route
+          element={
+            <RequireAuth>
+              <MainLayout />
+            </RequireAuth>
+          }
+        >
           <Route path="/home" element={<HomePage />} />
           <Route path="/map" element={<Map />} />
         </Route>
@@ -31,7 +48,14 @@ function AnimatedRoutes() {
         <Route path="/place/:id" element={<PlaceDetail />} />
         <Route path="/board/:id">
           <Route index element={<BoardDetail />} />
-          <Route path="postit" element={<PostItEditor />} />
+          <Route
+            path="postit"
+            element={
+              <RequireAuth>
+                <PostItEditor />
+              </RequireAuth>
+            }
+          />
         </Route>
 
         <Route path="*" element={<Navigate to="/splash" replace />} />
