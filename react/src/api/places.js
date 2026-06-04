e167a992b6de329d9501fe996ae36918fa6a8546
bm1611 <bm1611@naver.com>
2026-06-04 16:49:24 +0900

    인기장소 가져오는 로직 쿼리파라미터에 기간 추가

--- react/src/api/places.js
+++ react/src/api/places.js
@@ -1,12 +1,13 @@
 import { apiClient } from './client'
 
 // 사용자 위치 기준 주변 인기장소 조회
-export async function fetchPopularPlaces({ latitude, longitude, radius, district, limit = 5 } = {}) {
+export async function fetchPopularPlaces({ latitude, longitude, radius, district, period, limit = 5 } = {}) {
   return apiClient.get('/api/places/popular', {
     latitude,
     longitude,
     radius,
     district,
+    period,
     limit,
   })
 }
